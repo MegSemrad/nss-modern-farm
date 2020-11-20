bc15cc5cbde294b28f229709906e952abc73068c
MegSemrad <mlsemrad753@gmail.com>
2020-11-20 14:57:35 -0600

    added 6 different seed.js files and imported all to the main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,48 @@
 console.log("Welcome to the main module")
 
 import { createPlan } from './plan.js'
-const yearlyPlan = createPlan ()
-console.log(yearlyPlan)
+const yearlyPlan = createPlan ();
+console.log(yearlyPlan);
+
+
+
+// POTATO
+import { createPotato } from "./seeds/potato.js"
+
+const potatoSeed = createPotato()
+console.log(potatoSeed);
+
+
+// SUNFLOWER
+import { createSunflower } from "./seeds/sunflower.js"
+
+const sunflowerSeed = createSunflower()
+console.log(sunflowerSeed);
+
+
+// ASPARAGUS
+import { createAsparagus } from "./seeds/asparagus.js"
+
+const asparagusSeed = createAsparagus()
+console.log(asparagusSeed);
+
+
+// SOYBEAN
+import { createSoybean } from "./seeds/soybean.js"
+
+const soybeanSeed = createSoybean()
+console.log(soybeanSeed);
+
+
+//CORN
+import { createCorn } from "./seeds/corn.js"
+
+const cornSeed = createCorn()
+console.log(cornSeed);
+
+
+//WHEAT
+import { createWheat } from "./seeds/wheat.js"
+
+const wheatSeed = createWheat()
+console.log(wheatSeed);
